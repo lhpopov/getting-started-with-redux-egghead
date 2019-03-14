097a92507b80d260e2be450db8f0f7510aa32e9b
Lyubomir Popov <lyubomir.popov@MB32140.local>
2019-03-14 16:43:43 +0200

    Added Counter.js with initial simple reducer functions. Added Todos.js, where redux createStore is being used along with custom reducers. Also reducers composition pattern is being introducer here.

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+import { createStore } from 'redux';
 
 class Counter extends React.Component {
     render() {
@@ -16,3 +20,95 @@ class Counter extends React.Component {
 }
 
 export default Counter;
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const renderC = () => {
+  ReactDOM.render(
+        <Counter value={ store.getState() }
+              onIncrement={ () => 
+                store.dispatch({
+                  type: 'INCREMENT'
+                })
+              }
+
+              onDecrement={ () => 
+                store.dispatch({
+                  type: 'DECREMENT'
+                })
+              }
+            />,
+            document.getElementById('root'));
+};
+
+const store = createStore(counter);
+store.subscribe(renderC);
+
+renderC();
+
+const addCounter = (list) => {
+    return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+    return [
+        ...list.slice(0, index),
+        ...list.slice(index + 1)
+    ];
+};
+
+const incrementCounter = (list, index) => {
+    return [
+        ...list.slice(0, index),
+        list[index] + 1,
+        ...list.slice(index + 1)
+    ];
+};
+
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+
+    deepFreeze(listBefore);
+
+    expect(
+        addCounter(listBefore)
+    ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+    const listBefore = [3, 5, 8];
+    const listAfter = [3, 8];
+
+    deepFreeze(listBefore);
+
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+
+};
+
+const testIncrementCounter = () => {
+    const listBefore = [3, 5, 8];
+    const listAfter = [3, 6, 8];
+
+
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+
+console.log('All tests passed');
---
+++ src/Todos.js
@@ -0,0 +1,164 @@
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+import { createStore } from 'redux';
+
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return { id: action.id, text: action.text, completed: false };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+
+            return { ...state, completed: !state.completed }
+        default:
+            return state;
+    }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => { return todo(t, action) });
+        case 'REMOVE_TODO':
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Todo redux test'
+    };
+    const stateAfter = [{
+        id: 0,
+        text: 'Todo redux test',
+        completed: false
+    }];
+
+    deepFreeze(stateBefore);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+    const stateBefore = [{
+        id: 0,
+        text: 'Todo redux test',
+        completed: false
+    }, {
+        id: 1,
+        text: 'Todo redux test. Already done.',
+        completed: false
+    }];
+
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+
+    const stateAfter = [{
+        id: 0,
+        text: 'Todo redux test',
+        completed: false
+    }, {
+        id: 1,
+        text: 'Todo redux test. Already done.',
+        completed: true
+    }];
+
+    deepFreeze(stateBefore);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+const testRemoveTodo = () => {
+    const stateBefore = [{
+        id: 0,
+        text: 'Todo redux test',
+        completed: false
+    }, {
+        id: 1,
+        text: 'Todo redux test. Already done.',
+        completed: false
+    }];
+
+    const action = {
+        type: 'REMOVE_TODO',
+        id: 0
+    };
+
+    const stateAfter = [{
+        id: 1,
+        text: 'Todo redux test. Already done.',
+        completed: false
+    }];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+const visibilityFilter = (
+    state = 'SHOW_ALL',
+    action
+) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+    };
+};
+
+testAddTodo();
+testToggleTodo();
+testRemoveTodo();
+
+const store = createStore(todoApp);
+
+console.log('--- Initial state ---');
+console.log(store.getState());
+
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Yo, first todo'
+});
+
+console.log('--- Current state ---');
+console.log(store.getState());
+
+console.log('--- Dispatching SET_VISIBILITY_FILTER ---');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+
+console.log('--- Current state ---');
+console.log(store.getState());
