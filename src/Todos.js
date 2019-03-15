29e7c9e34e8a0d714822d7e6141f62d0ffb72e00
Lyubomir Popov <--global>
2019-03-15 16:42:49 +0200

    Decoupled Footer component. Added Link component and moved store.subscribe logic to FooterLink component.

--- src/Todos.js
+++ src/Todos.js
@@ -64,17 +64,85 @@ const AddTodo = ({
 };
 
 /* Footer component */
-const Footer = ({
-    visibilityFilter,
-    onFilterClick
-}) => {
+const Footer = () => {
     return (
         <p>
             Show: 
-            {' '}<FilterLink filter='SHOW_ALL' currentFilter={ visibilityFilter } onClick={ onFilterClick } >All</FilterLink>
-            {' '}<FilterLink filter ='SHOW_ACTIVE' currentFilter={ visibilityFilter } onClick={ onFilterClick } >Active</FilterLink>
-            {' '}<FilterLink filter ='SHOW_COMPLETED' currentFilter={ visibilityFilter } onClick={ onFilterClick } >Completed</FilterLink>
+            {' '}
+            <FilterLink 
+                filter='SHOW_ALL'
+            >
+                 All
+             </FilterLink>
+            {' '}
+            <FilterLink 
+                filter ='SHOW_ACTIVE'
+            >
+                 Active
+             </FilterLink>
+            {' '}
+            <FilterLink 
+                filter ='SHOW_COMPLETED'
+            >
+                 Completed
+             </FilterLink>
         </p>
+    );
+};
+
+/* FooterLink component */
+class FilterLink extends React.Component {
+    componentDidMount() {
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        const props = this.props;
+        const state = store.getState();
+
+        return (
+            <Link 
+                active={
+                    props.filter === state.visibilityFilter
+                }
+                onClick={() => {
+                        store.dispatch({
+                            type: 'SET_VISIBILITY_FILTER',
+                            filter: props.filter
+                        });
+                    }
+                }
+            >
+            {props.children}
+            </Link>
+        );
+    }
+};
+
+/* Link component */
+const Link = ({
+    active,
+    children,
+    onClick
+}) => {
+    if (active) {
+        return <span>{children}</span>
+    }
+
+    return (
+        <a href='#'
+            onClick={ e => {
+                e.preventDefault();
+                onClick();
+        }}>
+            {children}
+        </a>
     );
 };
 
@@ -95,28 +163,6 @@ const getVisibleTodos = (todos, filter) => {
     }
 };
 
-const FilterLink = ({
-    filter,
-    currentFilter,
-    children,
-    onClick
-}) => {
-    if (filter === currentFilter) {
-        return <span>{children}</span>
-    }
-
-    return (
-        <a href='#'
-            onClick={ e => {
-                    e.preventDefault();
-                    onClick(filter);
-                }
-            }>
-            {children}
-        </a>
-    );
-};
-
 const TodoApp = ({
     todos,
     visibilityFilter
@@ -131,27 +177,19 @@ const TodoApp = ({
                         });
                     } 
                 }
-                />
+                /> 
                 <TodoList 
-                    todos={ getVisibleTodos(todos, visibilityFilter) }
-                    onTodoClick={
-                         (id) => {
+                    todos = { getVisibleTodos(todos, visibilityFilter) } 
+                    onTodoClick = {
+                        (id) => {
                             store.dispatch({
                                 type: 'TOGGLE_TODO',
                                 id
                             });
                         }
-                    }                    
-                />
-                <Footer 
-                    visibilityFilter={ visibilityFilter}
-                    onFilterClick={ (filter) => {
-                        store.dispatch({
-                            type: 'SET_VISIBILITY_FILTER',
-                            filter
-                        });
-                    }}
-                />
+                    }
+                /> 
+            <Footer />
             </div>
         );
 }
